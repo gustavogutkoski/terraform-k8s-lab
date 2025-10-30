6cc518c456f1c490fb64a14dd74d2d3932ac72c2
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-10-30 13:32:09 -0300

    feat: add pods to trustscore-api and postgres 16

--- k8s-manifests/main.tf
+++ k8s-manifests/main.tf
@@ -1,7 +1,41 @@
 resource "kubernetes_manifest" "nginx_deploy" {
-  manifest = yamldecode(file("${path.module}/../manifests/deployment.yaml"))
+  manifest = yamldecode(file("${path.module}/../manifests/nginx/deployment.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx_svc" {
-  manifest = yamldecode(file("${path.module}/../manifests/service.yaml"))
+  manifest = yamldecode(file("${path.module}/../manifests/nginx/service.yaml"))
+}
+
+resource "kubernetes_manifest" "trustscore_api_deploy" {
+  manifest = yamldecode(file("${path.module}/../manifests/trustscore-api/deployment.yaml"))
+}
+
+resource "kubernetes_manifest" "trustscore_api_svc" {
+  manifest = yamldecode(file("${path.module}/../manifests/trustscore-api/service.yaml"))
+}
+
+resource "kubernetes_manifest" "postgres_configmap" {
+  manifest = yamldecode(file("${path.module}/../manifests/postgres/configmap.yaml"))
+}
+
+resource "kubernetes_manifest" "postgres_secret" {
+  manifest = yamldecode(file("${path.module}/../manifests/postgres/secret.yaml"))
+}
+
+resource "kubernetes_manifest" "postgres_pvc" {
+  manifest = yamldecode(file("${path.module}/../manifests/postgres/pvc.yaml"))
+}
+
+resource "kubernetes_manifest" "postgres_deploy" {
+  manifest = yamldecode(file("${path.module}/../manifests/postgres/deployment.yaml"))
+  
+  depends_on = [
+    kubernetes_manifest.postgres_configmap,
+    kubernetes_manifest.postgres_secret,
+    kubernetes_manifest.postgres_pvc
+  ]
+}
+
+resource "kubernetes_manifest" "postgres_svc" {
+  manifest = yamldecode(file("${path.module}/../manifests/postgres/service.yaml"))
 }
